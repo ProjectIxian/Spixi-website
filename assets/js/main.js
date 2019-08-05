bae8c35883d3fe41fdb77737306419407cda3da6
MarkoAT <no-reply@ixian.io>
2019-08-05 20:28:19 +0200

    fixed cookies bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
 
 function closeCookiePolicyBar()
 {
-    document.getElementById("cookiePolicyBar").toggleDisplay(false);
+    document.getElementById("cookiePolicyBar").style.display = "none";
     document.cookie = "ixianCookieAgreed=1; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
 }
 
